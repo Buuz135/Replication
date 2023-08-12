4e28ea337bd497e481bf86334cc1437b40a4c8f4
Buuz135 <buuz135@hotmail.com>
2023-08-12 18:15:36 +0200

    Added helper methods to add matter tanks

--- src/main/java/com/buuz135/replication/block/tile/NetworkBlockEntity.java
+++ src/main/java/com/buuz135/replication/block/tile/NetworkBlockEntity.java
@@ -1,5 +1,6 @@
 package com.buuz135.replication.block.tile;
 
+import com.buuz135.replication.api.matter_fluid.component.MatterTankComponent;
 import com.buuz135.replication.api.network.NetworkElement;
 import com.buuz135.replication.network.NetworkManager;
 import com.buuz135.replication.network.element.type.DefaultMatterNetworkElement;
@@ -12,11 +13,26 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class NetworkBlockEntity<T extends ActiveTile<T>> extends ActiveTile<T> implements ITickableBlockEntity<T> {
 
+    private List<MatterTankComponent<T>> matterTankComponents;
+
     public NetworkBlockEntity(BasicTileBlock<T> base, BlockEntityType<?> blockEntityType, BlockPos pos, BlockState state) {
         super(base, blockEntityType, pos, state);
+        this.matterTankComponents = new ArrayList<>();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void initClient() {
+        super.initClient();
+        this.matterTankComponents.forEach(matterTankComponent -> matterTankComponent.getScreenAddons().forEach(this::addGuiAddonFactory));
     }
 
     @Override
@@ -29,6 +45,15 @@ public abstract class NetworkBlockEntity<T extends ActiveTile<T>> extends Active
                 networkManager.addElement(createElement(level, worldPosition));
             }
         }
+    }
+
+    public void addMatterTank(MatterTankComponent<T> matterTankComponent){
+        this.matterTankComponents.add(matterTankComponent);
+        matterTankComponent.setComponentHarness(this.getSelf());
+        matterTankComponent.getContainerAddons().forEach(this::addContainerAddonFactory);
+        matterTankComponent.setOnContentChange(() -> {
+           syncObject(matterTankComponent);
+        });
     }
 
     private boolean unloaded;
